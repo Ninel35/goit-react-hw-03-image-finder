e54d7d899ca5b35cb964030d3a337fbaf3518361
Ninel35 <134783428+Ninel35@users.noreply.github.com>
2023-12-11 21:58:22 +0100

    homeWork

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,29 +2,43 @@ import { Component } from "react";
 import { Searchbar } from "./Searchbar";
 import { ImageGallery } from "./ImageGallery";
 import { getAllImages } from "api/allImages";
+import { Loader } from "./Loader";
 
 
 export class App extends Component {
   state = {
     images: null,
-    filter: ""
+    filter: "",
+    page: 1,
+    isLoading: false,
+    error: ''
+
   }
 
   componentDidMount() {
        this.getImages()
   }
 
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.filter !== prevState.filter) {
+  componentDidUpdate(_, prevState) {
+    if (this.state.filter !== prevState.filter || prevState.page !== this.state.page) {
       this.getImages()
     }
   }
 
   getImages = async () => {
+    try {
+      this.setState({isLoading: true})
     const response = await getAllImages(this.state.filter)
-    this.setState({
-      images: response.hits
-    })
+    this.setState({images: response.hits})
+  
+} catch (error) {
+  this.setState({error: error.message})
+    } finally {
+      this.setState({isLoading: false})
+      
+}
+
+  
   }
 
   handleSubmit = (evt) => {
@@ -35,9 +49,13 @@ export class App extends Component {
   }
 
   render() {
+    const {isLoading, error} = this.state
     return (
       <>
         <Searchbar handleSubmit={this.handleSubmit} />
+        {isLoading && <Loader />}
+        {error && <h2>{error}</h2>}
+        
         <ImageGallery images={this.state.images} />
       </>
     
